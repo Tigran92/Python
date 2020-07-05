e3b10bc4d150dfc853cbbdb412b3997ee3d90035
Tigran92 <22913312+Tigran92@users.noreply.github.com>
2020-07-05 11:43:01 +0100

    Scrabble project-1

--- dictionaries/scrabble_project/script.py
+++ dictionaries/scrabble_project/script.py
@@ -10,9 +10,27 @@ print(letter_to_points)
 def score_word(word):
 
   point_total = 0
-  for i in word:
-    point_total += letter_to_points.get(i,0)
+  for letter in word:
+    point_total += letter_to_points.get(letter,0)
   return point_total
 
 print_total_points = score_word("WoRd")
 print(print_total_points)
+
+brownie_points = score_word("BROWNIE")
+print(brownie_points)
+
+
+player_to_words = {"player1": ["BLUE", "TENNIS", "EXIT"], "wordNerd": ["EARTH", "EYES", "MACHINE"], "Lexi Con": ["ERASER", "BELLY", "HUSKY"], "Prof Reader": ["ZAP", "COMA", "PERIOD"]}
+
+player_to_points = {}
+
+for player, words in player_to_words.items():
+
+  player_points = 0
+  for word in words:
+
+    player_points += score_word(word)
+  player_to_points[player] = player_points
+
+print(player_to_points)
