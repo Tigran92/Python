3bd2fd1fe8a85e1c4e6fcb3e9c8506187ce1648e
Tigran92 <22913312+Tigran92@users.noreply.github.com>
2020-07-13 23:13:26 +0100

    Hacking The Fender project

--- files/hacking_the_fender/script.py
+++ files/hacking_the_fender/script.py
@@ -25,3 +25,34 @@ with open(path, 'w') as compromised_user_file:
   for users in compromised_users:
 
     compromised_user_file.write(users)
+
+import json
+
+path = os.path.join(my_path, "../hacking_the_fender/boss_message.json")
+with open(path, 'w') as boss_message:
+
+  boss_message_dict = {"recipient": "The Boss", "message": "Mission Success"}
+
+  json.dump(boss_message_dict, boss_message)
+
+path = os.path.join(my_path, "../hacking_the_fender/new_passwords.json")
+with open(path, 'w') as new_passwords_obj:
+
+  slash_null_sig = """_  _     ___   __  ____
+/ )( \   / __) /  \(_  _)
+) \/ (  ( (_ \(  O ) )(
+\____/   \___/ \__/ (__)
+ _  _   __    ___  __ _  ____  ____
+/ )( \ / _\  / __)(  / )(  __)(    \
+) __ (/    \( (__  )  (  ) _)  ) D (
+\_)(_/\_/\_/ \___)(__\_)(____)(____/
+        ____  __     __   ____  _  _
+ ___   / ___)(  )   / _\ / ___)/ )( \
+(___)  \___ \/ (_/\/    \\___ \) __ (
+       (____/\____/\_/\_/(____/\_)(_/
+ __ _  _  _  __    __
+(  ( \/ )( \(  )  (  )
+/    /) \/ (/ (_/\/ (_/\
+\_)__)\____/\____/\____/"""
+
+  new_passwords_obj.write(slash_null_sig)
