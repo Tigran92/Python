b3f9baba7b603f428dbd434e80dcd7ef206aa8d5
Tigran92 <22913312+Tigran92@users.noreply.github.com>
2020-07-15 23:42:03 +0100

    Classes-1

--- classes/classes/script.py
+++ classes/classes/script.py
@@ -75,3 +75,84 @@ how_many_s = [{'s': False}, "sassafrass", 18, ["a", "c", "s", "d", "s"]]
 for element in how_many_s:
   if hasattr(element, "count"):
     print(element.count("s"))
+
+
+# Self
+class Circle:
+  pi = 3.14
+  def __init__(self, diameter):
+    print("Creating circle with diameter {d}".format(d=diameter))
+    # Add assignment for self.radius here:
+
+    self.radius = diameter / 2
+
+  def circumference(self):
+    return 2 * self.pi * self.radius
+
+medium_pizza1 = Circle(12)
+teaching_table1 = Circle(36)
+round_room1 = Circle(11460)
+
+print(medium_pizza1.circumference())
+print(teaching_table1.circumference())
+print(round_room1.circumference())
+
+
+# Everything is an Object
+check_dir = dir(5)
+print(check_dir)
+
+def this_function_is_an_object(test):
+
+  return "hello"+test
+
+print(dir(this_function_is_an_object))
+
+# String Representation
+class Circle:
+  pi = 3.14
+
+  def __init__(self, diameter):
+    self.radius = diameter / 2
+
+  def area(self):
+    return self.pi * self.radius ** 2
+
+  def circumference(self):
+    return self.pi * 2 * self.radius
+
+  def __repr__(self):
+    return "Circle with radius {radius}".format(radius=self.radius)
+
+
+medium_pizza = Circle(12)
+teaching_table = Circle(36)
+round_room = Circle(11460)
+
+print(medium_pizza)
+print(teaching_table)
+print(round_room)
+
+
+class Student:
+
+  def __init__(self, name, year):
+
+    self.name = name
+    self.year = year
+    self.grades = []
+
+  def add_grade(self, grade):
+    if type(grade) is Grade:
+      self.grades.append(grade)
+
+class Grade:
+  minimum_passing = 65
+
+  def __init__(self, score):
+    self.score = score
+
+roger = Student("Roger van der Weyden", 10)
+sandro = Student("Sandro Botticelli", 12)
+pieter = Student("Pieter Bruegel the Elder", 8)
+pieter.add_grade(Grade(100))
