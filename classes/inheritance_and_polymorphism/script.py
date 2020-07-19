7804c53270ed4066d131de774756ad86d0c9bc3a
Tigran92 <22913312+Tigran92@users.noreply.github.com>
2020-07-19 23:32:21 +0100

    Inheritance and Polymorphism-2

--- classes/inheritance_and_polymorphism/script.py
+++ classes/inheritance_and_polymorphism/script.py
@@ -27,7 +27,7 @@ candle_shop.buy('blue')
 # This should raise OutOfStock:
 # candle_shop.buy('green')
 
-
+# Overriding Methods
 class Message:
   def __init__(self, sender, recipient, text):
     self.sender = sender
@@ -45,3 +45,16 @@ class User:
 class Admin(User):
   def edit_message(self, message, new_text):
     message.text = new_text
+
+
+# Super()
+class PotatoSalad:
+  def __init__(self, potatoes, celery, onions):
+    self.potatoes = potatoes
+    self.celery = celery
+    self.onions = onions
+
+class SpecialPotatoSalad(PotatoSalad):
+  def __init__(self, potatoes, celery, onions):
+    super().__init__(potatoes, celery, onions)
+    self.raisins = 40
