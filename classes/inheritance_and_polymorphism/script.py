8db7745bd36bcc4202a86cc877f3f01de40151cf
Tigran92 <22913312+Tigran92@users.noreply.github.com>
2020-07-20 23:29:43 +0100

    Inheritance and Polymorphism-3

--- classes/inheritance_and_polymorphism/script.py
+++ classes/inheritance_and_polymorphism/script.py
@@ -58,3 +58,17 @@ class SpecialPotatoSalad(PotatoSalad):
   def __init__(self, potatoes, celery, onions):
     super().__init__(potatoes, celery, onions)
     self.raisins = 40
+
+
+# Interfaces
+class InsurancePolicy:
+  def __init__(self, price_of_item):
+    self.price_of_insured_item = price_of_item
+
+class VehicleInsurance(InsurancePolicy):
+  def get_rate(self):
+    return self.price_of_insured_item * .001
+
+class HomeInsurance(InsurancePolicy):
+  def get_rate(self):
+    return self.price_of_insured_item * .00005
