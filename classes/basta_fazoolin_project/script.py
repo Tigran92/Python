1d1387a9e4723fd16f7a2cc533ca50190a18e241
Tigran92 <22913312+Tigran92@users.noreply.github.com>
2020-07-26 23:18:08 +0100

    Basta Fazoolin' project-1

--- classes/basta_fazoolin_project/script.py
+++ classes/basta_fazoolin_project/script.py
@@ -2,4 +2,17 @@ class Menu:
 
   def __init__(self, name, items, start_time, end_time):
 
+    self.name = name
+    self.items = items
+    self.start_time = start_time
+    self.end_tiem = end_time
+
+
+
+brunch_items = {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50}
+
+brunch = Menu('Brunch', brunch_items, 1100, 1600)
+
+print(brunch.name)
+
     
